ff72379822d7cd99fa2cf24e9ff46887ce433fdc
ahm3dDarwish <83702775+ahm3dDarwish@users.noreply.github.com>
2021-05-05 12:47:55 +0200

    Add files via upload

---
+++ nmapscan.sh
@@ -0,0 +1,23 @@
+echo "################################################################"
+
+echo "This Script Dedicate To My Geinus instructor Bishoy Gamel "
+
+
+echo "################################################################"
+
+echo "Script By Ahmed Darwish"
+
+echo "#################################################################"
+
+echo "Enter IP"
+
+read ip
+
+echo "Enter Port To Scan It"
+read port
+
+nmap --mtu 16 $ip
+
+xterm -e nmap -sC -f -f -f -oN Scan.txt $ip
+
+xterm -e nmap --mtu 8 -p $port -oN PortResult.txt $ip
